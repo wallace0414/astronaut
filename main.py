4aeb012f1b4fbe8586db910da68215a5aebb43e1
wallace0414 <nagy.daniel0414@gmail.com>
2024-02-07 21:32:12 +0100

    második mentés

--- main.py
+++ main.py
@@ -24,16 +24,20 @@ def szamlal(honapok_lista):
         honapok_szama[index] = int(honapok_szama[index]) + 1
     return honapok_szama
 
-def elso_harom(honapok_szama, x):
-    honapok = ['Január', 'Február', 'Március', 'Április', 'Május', 'Június', 'Július', 'Augusztus', 'Szeptember', 'Október', 'November', 'December']
+
+def elso_harom(honapok_szama, szemelyek_szama):
+    honapok = ['Január', 'Február', 'Március', 'Április', 'Május', 'Június', 'Július', 'Augusztus', 'Szeptember',
+               'Október', 'November', 'December']
     sorrend = sorted(zip(honapok_szama, honapok), reverse=True)
-    print(f'1. {sorrend[0][1]}  {(sorrend[0][0] / x * 100):.1f}%')
-    print(f'2. {sorrend[1][1]}  {(sorrend[1][0] / x * 100):.1f}%')
-    print(f'3. {sorrend[2][1]}  {(sorrend[2][0] / x * 100):.1f}%')
+    print(f'1. {sorrend[0][1]}  {(sorrend[0][0] / szemelyek_szama * 100):.1f}%')
+    print(f'2. {sorrend[1][1]}  {(sorrend[1][0] / szemelyek_szama * 100):.1f}%')
+    print(f'3. {sorrend[2][1]}  {(sorrend[2][0] / szemelyek_szama * 100):.1f}%')
+
 
 def main():
     honapok_lista = felbont(beolvas())
     honapok_szama = szamlal(honapok_lista)
     elso_harom(honapok_szama, len(honapok_lista))
+
 
 main()
